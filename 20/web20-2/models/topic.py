79b60d6a7b4700fb0ad2566bd4a091a7a843c1d0
wangjialong-001 <2785188932@qq.com>
2018-09-15 10:20:53 +0800

    .

---
+++ 20/web20-2/models/topic.py
@@ -0,0 +1,69 @@
+import time
+from models import Model
+from models.user import User
+from models.mongua import Mongua
+
+
+class Topic(Model):
+    @classmethod
+    def get(cls, id):
+        m = cls.find_by(id=id)
+        m.views += 1
+        m.save()
+        return m
+
+    def __init__(self, form):
+        self.id = None
+        self.views = 0
+        self.title = form.get('title', '')
+        self.content = form.get('content', '')
+        self.ct = int(time.time())
+        self.ut = self.ct
+        self.user_id = form.get('user_id', '')
+        self.board_id = int(form.get('board_id', -1))
+
+    def replies(self):
+        from .reply import Reply
+        ms = Reply.find_all(topic_id=self.id)
+        return ms
+
+    def board(self):
+        from .board import Board
+        m = Board.find(self.board_id)
+        return m
+
+    def user(self):
+        u = User.find(id = self.user_id)
+        return u
+
+
+class Topic(Mongua):
+    __fields__ = Mongua.__fields__ + [
+        ('content', str, ''),
+        ('user_id', int, -1),
+        ('board_id', int, -1),
+        ('views', int, 0)
+    ]
+
+    @classmethod
+    def get(cls, id):
+        m = cls.find_by(id=id)
+        m.views += 1
+        m.save()
+        return m
+
+    def replies(self):
+        from .reply import Reply
+        ms = Reply.find_all(topic_id=self.id)
+        return ms
+
+    def board(self):
+        from .board import Board
+        m = Board.find(self.board_id)
+        return m
+
+    def user(self):
+        u = User.find(id = self.user_id)
+        return u
+
+
---
+++ 23/23.py
@@ -0,0 +1,44 @@
+'''
+1. vagrant
+    openstack
+    docker
+2. 架构
+web application
+SOA
+micro service
+    2.1 不同服务tcp 语言的异构
+    2.2 数据接口api， 前端渲染，前后端渲染混合
+    2.3 自动弹性，容器，虚拟化，LaaS PaaS
+3. 分布式
+CAP猜想->CAP定理
+C 一致性
+A 可用性
+P 网络分区的可容忍性
+    3.1. 小米
+    3.2. 电商 做秒杀
+4. 后端现有架构图
+
+5. jsonrpc ? xmlrpc? soap ? restful api?
+jsonrpc http json result['result']
+xmlrpc http xml
+soap
+restful api http method
+6. 性能的代价
+jsonrpc ? 缺点
+ 6.1 overhead
+ 6.2 单工
+         好处
+ 6.3 简单
+ 6.4 乱序
+
+ tcp 双工？
+7. websocket js socket socket-io
+
+8. 聊天室
+1. 聊天，room，对于room广播
+2. 加入房间，退出房间
+3. jquery
+4. socket.io
+5. socket.io server
+6. tcp socket io session
+'''
